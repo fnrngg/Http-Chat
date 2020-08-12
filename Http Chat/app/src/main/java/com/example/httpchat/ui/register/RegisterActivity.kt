5b225de8a595cf7c3ec02fc5389772bc7434d234
fnrngg <balsh17@freeuni.edu.ge>
2020-08-12 12:30:38 +0400

    Fixed image load crash issue

--- Http Chat/app/src/main/java/com/example/httpchat/ui/register/RegisterActivity.kt
+++ Http Chat/app/src/main/java/com/example/httpchat/ui/register/RegisterActivity.kt
@@ -7,7 +7,6 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Base64
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.httpchat.databinding.ActivityRegisterBinding
@@ -89,8 +88,8 @@ class RegisterActivity : AppCompatActivity(), RegisterContract.View {
     }
 
     override fun startActivity() {
-//        Log.d("ragaca","ragaca")
         MessagesActivity.start(this)
+        finish()
     }
 
 }
